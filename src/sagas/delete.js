0ee6c76f4125ef36652e1469cf7a582281201398
Rushabh Gothi <rushg171.gmail.com>
2020-03-19 17:24:14 +0530

    url update

--- src/sagas/delete.js
+++ src/sagas/delete.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetchData = async () => {
   const result = await axios.get(
-    "https://pigeon-backend-9909.herokuapp.com/delete"
+    "https://pigeon-backend-9909.herokuapp.com/procedure/delete"
   );
   const procedreData = result.data.data;
   console.log(procedreData);
