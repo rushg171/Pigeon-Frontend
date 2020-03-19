51686023d674e534e00876aaf4ff5ee5c2039e58
Rushabh Gothi <rushg171.gmail.com>
2020-03-19 16:19:13 +0530

    fetchdata url changed

--- src/sagas/fetchData.js
+++ src/sagas/fetchData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetchData = async () => {
   const result = await axios.get(
-    "https://stumy-backend-8988.herokuapp.com/procedure/all"
+    "https://pigeon-backend-9909.herokuapp.com/procedure/all"
   );
   const procedreData = result.data.data;
   console.log(procedreData);
